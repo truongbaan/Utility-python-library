27118d123454f341b6e59776e31280df542b40c3
An Truong <truongbaansoftware@gmail.com>
2025-05-18 22:25:10 +0700

    add class to init for easier import
    thinking of adding an simple AI for yes no question

--- freeai_utils/__init__.py
+++ freeai_utils/__init__.py
@@ -1,6 +1,9 @@
 from .audio_record import WavRecorder, MP3Recorder, check_wav_length_and_size, check_mp3_length_and_size
 from .audio_to_text_whisper import OpenAIWhisper
+from .audio_to_text_vn import VN_Whisper
 from .clean_text_for_tts import clean_ai_text_for_tts
+from .cleaner import __Cleaner
+from .document_filter import DocumentFilter
 from .geminiAPI import GeminiClient
 from .google_search import GoogleSearcher
 from .image_to_text import ImageCaptioner
@@ -8,18 +11,20 @@ from .text_from_image_easyocr import Text_Extractor_EasyOCR
 from .text_to_speech_gtts import gtts_print_supported_languages, gtts_speak
 from .text_to_speech_pyttsx3 import Text_To_Speech_Pyttsx3
 from .pdf_docx_reader import PDF_DOCX_Reader
-from .document_filter import DocumentFilter
+
 
 __all__ = [
     "WavRecorder", "MP3Recorder","check_wav_length_and_size","check_mp3_length_and_size"
     "OpenAIWhisper",
+    "VN_Whisper",
     "clean_ai_text_for_tts",
+    "__Cleaner",
+    "DocumentFilter",
     "GeminiClient",
     "GoogleSearcher",
     "ImageCaptioner",
     "Text_Extractor_EasyOCR",
     "gtts_print_supported_languages", "gtts_speak",
     "Text_To_Speech_Pyttsx3",
-    "PDF_DOCX_Reader",
-    "DocumentFilter"
+    "PDF_DOCX_Reader"
 ]
