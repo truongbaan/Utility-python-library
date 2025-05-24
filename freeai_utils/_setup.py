89b1ff08b53452c0d047cb3093b803e96eae7994
An Truong <truongbaansoftware@gmail.com>
2025-05-24 12:01:49 +0700

    break to smaller install in order to reduce the cpu usage, also for multiple command possible

--- freeai_utils/_setup.py
+++ freeai_utils/_setup.py
@@ -1,25 +1,54 @@
+import gc
+
 def install_default_model():
-    decision = input("""This function will download all default models used by this library. 
-The process can take a significant amount of time, so feel free to take a break.
-Would you like to proceed with the download? (Y/n): """)
-    if decision.lower() == "y":
-        from .audio_to_text_vn import VN_Whisper
-        from .audio_to_text_whisper import OpenAIWhisper
-        from .decider import DecisionMaker
-        from .document_filter import DocumentFilter
-        from .image_to_text import ImageCaptioner
-        from .text_from_image_easyocr import Text_Extractor_EasyOCR
-        from .language_detection import MBartTranslator, M2M100Translator
-        vnwhisper = VN_Whisper()
-        whisper = OpenAIWhisper()
-        dec = DecisionMaker()
-        doc = DocumentFilter(auto_init=False)
-        imgdes = ImageCaptioner()
-        textocr = Text_Extractor_EasyOCR()
-        mbart = MBartTranslator()
-        m2m100 = M2M100Translator()
-    else:
+    decision = input(
+        "This function will download all default models used by this library.\n"
+        "The process can take a significant amount of time, so feel free to take a break.\n"
+        "Would you like to proceed with the download? (Y/n): ").strip().lower()
+        
+    if decision != "y":
         print("Download cancelled.")
-       
+        return
+        
+    download_transcription()
+    download_document_related()
+    download_image_related()
+    download_translation()
+
+def download_transcription():
+    from .audio_to_text_vn import VN_Whisper
+    from .audio_to_text_whisper import OpenAIWhisper
+     
+    _download_and_purge(VN_Whisper)
+    _download_and_purge(OpenAIWhisper)
+
+
+def download_document_related():
+    from .decider import DecisionMaker
+    from .document_filter import DocumentFilter
+     
+    _download_and_purge(DecisionMaker)
+    _download_and_purge(DocumentFilter, auto_init=False)
+
+
+def download_image_related():
+    from .image_to_text import ImageCaptioner
+    from .text_from_image_easyocr import Text_Extractor_EasyOCR
+    
+    _download_and_purge(ImageCaptioner)
+    _download_and_purge(Text_Extractor_EasyOCR)
+
+
+def download_translation():
+    from .language_detection import MBartTranslator, M2M100Translator
+    
+    _download_and_purge(MBartTranslator)
+    _download_and_purge(M2M100Translator)
+
+def _download_and_purge(cls, *args, **kwargs):
+    inst = cls(*args, **kwargs)
+    del inst
+    gc.collect()
+
 if __name__ == "__main__":
     install_default_model()
