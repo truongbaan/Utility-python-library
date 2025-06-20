ca6b40d6cee7aeacec9453a450c525cbdf4dfee5
An Truong <truongbaansoftware@gmail.com>
2025-06-19 23:43:30 -0700

    Update cli.py
    create a function for removing extra files, support in cleaning the libraries before pip uninstall

--- freeai_utils/cli.py
+++ freeai_utils/cli.py
@@ -1,13 +1,13 @@
 # freeai_utils/cli.py
 import sys
 import click
-from ._setup import install_model
+from ._setup import install_model, remove_dir
+import os
 
 @click.group(invoke_without_command=True)
 @click.pass_context
 def main(ctx):
     if ctx.invoked_subcommand is None:
-        # no subcommand given: show help and exit with error
         click.echo(ctx.get_help())
         sys.exit(1)
 
@@ -32,6 +32,44 @@ def help():
     print("   ICF Image generator models      →  freeai-utils setup ICF")
     print("   ICE Embeded for Image generator models      →  freeai-utils setup ICE")
     print("*" * 100)
-
-if __name__ == "__main__":
-    main()
+    print("Usage: freeai-utils clean\n")
+    print("Description: Clean up downloaded files and model through setup command")
+    print("*" * 100)
+    
+@main.command(help="Remove extra downloaded files")
+def clean():
+    decision = input(
+        "This function will remove all downloaded files from civitai as well as vosk model\n"
+        "This should only be used when you want to clean up the libraries before pip uninstall\n"
+        "Are you sure you want to proceed? (Y/n): ").strip().lower()
+        
+    if decision != "y":
+        print("Clean Up cancelled.")
+        return
+    
+    cur_dir = os.path.dirname(os.path.abspath(__file__))
+    
+    #known folder
+    known_folder = ["downloaded_models", "vosk_model"]
+    for folder in known_folder:
+        path = os.path.join(cur_dir, folder)
+        try:
+            remove_dir(path=path)
+        except FileNotFoundError:
+            pass
+        except Exception as e:
+            raise Exception(e)
+    
+    unknown_item = []
+    #unknown folder in the directory
+    for item_name in os.listdir(cur_dir):
+            item_path = os.path.join(cur_dir, item_name)
+            if not os.path.isdir(item_path):
+                continue
+            unknown_item.append(item_path)
+            
+    if len(unknown_item) > 0:
+        print(f"Warning. Unknowned folder found, did you personally add extra folders? If not, please go to {cur_dir} to delete manually")
+        print(f"Unknown folder amount: {len(unknown_item)}")
+        for item in unknown_item:
+            print(f"Path : {item}") 
