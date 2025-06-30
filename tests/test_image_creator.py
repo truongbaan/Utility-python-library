86ca787b96caf70bdbb93139a4649a782161ec60
An Truong <truongbaansoftware@gmail.com>
2025-06-30 16:34:24 +0700

    Complete test cases

---
+++ tests/test_image_creator.py
@@ -0,0 +1,84 @@
+import pytest
+import gc
+from freeai_utils.image_creator import SDXL_TurboImage, SD15_Image
+
+def test_init_sdxl(): #idk what to test...
+    model = SDXL_TurboImage()
+    assert model.model is not None
+    assert model.device in ("cpu", "cuda")
+    del model
+    gc.collect()
+
+@pytest.fixture(scope="module")
+def sd15():
+    model = SD15_Image(embed_default=False)
+    yield model
+    del model
+    gc.collect()
+
+def test_init_sd15(sd15):
+    assert sd15.model is not None
+    assert sd15.device in ("cpu", "cuda")
+    assert sd15.model.safety_checker is not None
+    assert sd15.model.feature_extractor is not None
+    #because embed default is false
+    embeds = ["easynegative", "badprompt", "negativehand"]
+    for embed in embeds:
+        assert embed not in sd15.model.tokenizer.added_tokens_encoder
+    assert sd15._model.get_active_adapters() == []
+
+def test_scheduler_sd15_sdekarras(sd15):
+    sd15._custom_scheduler("SDE Karras")
+    from diffusers import DPMSolverSinglestepScheduler
+    # build the DPM++ SDE Karras scheduler
+    new_scheduler  = DPMSolverSinglestepScheduler.from_config( #this requires number of step to be even
+        sd15._model.scheduler.config,
+        use_karras_sigmas=True,
+        lower_order_final=True
+    )
+    assert sd15.model.scheduler.config == new_scheduler.config
+
+def test_scheduler_sd15_dpmkarras(sd15):
+    sd15._custom_scheduler("DPM++ 2M Karras")
+    from diffusers import DPMSolverMultistepScheduler
+    new_scheduler  = DPMSolverMultistepScheduler.from_config( 
+            sd15._model.scheduler.config,
+            use_karras_sigmas=True,
+            solver_order=2,
+            algorithm_type="dpmsolver++",
+            lower_order_final=True 
+        )
+    assert sd15.model.scheduler.config == new_scheduler.config
+    
+def test_scheduler_sd15_euler_a(sd15):
+    sd15._custom_scheduler("Euler A")
+    from diffusers import EulerAncestralDiscreteScheduler
+    new_scheduler  = EulerAncestralDiscreteScheduler.from_config(sd15._model.scheduler.config, use_karras_sigmas=True,
+        lower_order_final = True)
+    assert sd15.model.scheduler.config == new_scheduler.config
+
+def test_load_default_embed_lora_sd15(sd15):
+    sd15._load_default_embed_and_lora()
+    embeds = ["easynegative", "badprompt", "negativehand"]
+    for embed in embeds:
+        assert embed in sd15.model.tokenizer.added_tokens_encoder
+    assert sd15._model.get_active_adapters() == ["add_detail"]
+
+def test_enable_safety_sd15(sd15):
+    sd15.enable_safety()
+    from diffusers.pipelines.stable_diffusion.safety_checker import StableDiffusionSafetyChecker #safety check
+    from transformers import CLIPImageProcessor #for safety check
+    safety_checker = StableDiffusionSafetyChecker.from_pretrained(
+        "CompVis/stable-diffusion-safety-checker"
+    ).to(sd15._device)
+    feature_extractor = CLIPImageProcessor.from_pretrained(
+        "openai/clip-vit-base-patch32"
+    )
+    assert sd15._model.safety_checker.config.to_dict() == safety_checker.config.to_dict()
+    assert sd15._model.feature_extractor.to_dict() == feature_extractor.to_dict()
+
+def disable_safety_sd15(sd15):
+    sd15.disable_safety()
+    assert sd15.model.safety_checker is None
+    assert sd15.model.feature_extractor is None
+    
---
+++ tests/test_language_detection.py
@@ -0,0 +1,52 @@
+import pytest
+import gc
+from freeai_utils.language_detection import LangTranslator, LocalTranslator, M2M100Translator, MBartTranslator
+
+def test_langtrans():
+    model = LangTranslator()
+    assert model.local_status == "backup"
+    assert model.local_translator is not None
+    assert model.translate(text_to_translate="Xin chào bạn", tgt_lang='en') == "Hello"
+    del model
+    gc.collect()
+
+def test_langtrans_local():
+    model = LangTranslator(local_status="inactive")
+    assert model.local_status is None
+    assert model.local_translator is None
+    assert model.translate(text_to_translate="Xin chào bạn", tgt_lang='en') == "Hello"
+    del model
+    gc.collect()
+
+def test_langtrans_fail():
+    with pytest.raises(ValueError, match="local_status could only be \'active\', \'inactive\', \'backup\'. Current value: unknown"):
+        model = LangTranslator(local_status="unknown")
+
+def test_localtrans():
+    model = LocalTranslator()
+    assert model.model is not None
+    assert model.translate(prompt="Xin chào bạn", tgt_lang='en') == "Hello friends."
+    del model
+    gc.collect()
+
+def test_localtrans_fail():
+    with pytest.raises(ValueError):
+        model = LocalTranslator(local_model_num=3)
+
+def test_m2():
+    model = M2M100Translator()
+    assert model.model is not None
+    assert model.tokenizer is not None
+    assert model.device in ("cpu", "cuda")
+    assert model.translate(text="Xin chào bạn", tgt_lang='en') == "Hello friends."
+    del model
+    gc.collect()
+
+def test_mbart():
+    model = MBartTranslator()
+    assert model.model is not None
+    assert model.tokenizer is not None
+    assert model.device in ("cpu", "cuda")
+    assert model.translate(text="Xin chào bạn", tgt_lang='en') == "Hi there."
+    del model
+    gc.collect()
