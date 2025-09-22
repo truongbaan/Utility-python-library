bd5c52aacacd53b5a935f891d9a3d4a73e5364ba
An Truong <truongbaansoftware@gmail.com>
2025-09-22 18:09:42 +0700

    modify the test to adapt to new lib

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -15,7 +15,16 @@ def test_model_initialized(webscrap):
     assert webscrap.user_agent == "Mozilla/5.0"
 
 def test_search(webscrap):
-    result = webscrap.search("What is the capital of Vietnam?")
+    tries = 0
+    result = ""
+    while "ha noi" not in result.lower() and "hanoi" not in result.lower() and tries < 3:
+        result = webscrap.search("What is the capital of Vietnam?")
+        tries += 1
+    tries = 0
+    result2 = ""
+    while "ha noi" not in result2.lower() and "hanoi" not in result2.lower() and tries < 3:
+        result2 = webscrap.quick_search("What is the capital of Vietnam?")
+        tries += 1
+    
     assert "ha noi" in result.lower() or "hanoi" in result.lower()
-    result2 = webscrap.quick_search("What is the capital of Vietnam?")
     assert "ha noi" in result2.lower() or "hanoi" in result2.lower()
